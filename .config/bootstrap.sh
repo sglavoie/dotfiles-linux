9e34878bcca30ed0b698ec38dcce6800dece56cc
Sébastien Lavoie <sgdlavoie@gmail.com>
2020-05-23 09:01:53 -0500

    Add bootstrap file to perform symbolic links
    * Useful when setting up new machines.

---
+++ .config/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Bash
+ln -sf ~/Dropbox/.custom/bash/.bash_aliases ~/.bash_aliases
+
+# Fonts
+ln -nsf ~/Dropbox/.custom/fonts ~/.fonts
+
+# Git
+ln -sf ~/Dropbox/.custom/.gitconfig ~/.gitconfig
+ln -sf ~/Dropbox/.custom/.gitconfig-github ~/.gitconfig-github
+ln -sf ~/Dropbox/.custom/.gitconfig-datopian ~/.gitconfig-datopian
