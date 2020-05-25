bb8a2fb8a1fe9a7bae21ecf6dd75546d7183a0f0
Sébastien Lavoie <sgdlavoie@gmail.com>
2020-05-24 21:38:18 -0500

    Remove duplicate bootstrap.sh
      * Avoid references to Dropbox in config.

--- Dropbox/.custom/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Bash
-ln -sf ~/Dropbox/.custom/bash/.bash_aliases ~/.bash_aliases
-ln -nsf ~/Dropbox/.custom/fonts ~/.fonts
-ln -sf ~/Dropbox/.custom/.gitconfig ~/.gitconfig
-ln -sf ~/Dropbox/.custom/.gitconfig-github ~/.gitconfig-github
-ln -sf ~/Dropbox/.custom/.gitconfig-datopian ~/.gitconfig-datopian
