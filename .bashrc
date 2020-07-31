8cd211e4a0a466e788fbeb9ea233c564b97031c8
Sébastien Lavoie <sgdlavoie@gmail.com>
2020-07-30 21:05:32 -0500

    Update username used for FZF config with Bash

--- .bashrc
+++ .bashrc
@@ -137,3 +137,5 @@ ex ()
     echo "'$1' is not a valid file"
   fi
 }
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/sebastien/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/sebastien/.fzf/bin"
+if [[ ! "$PATH" == */home/sglavoie/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/sglavoie/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/sebastien/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/sglavoie/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/sebastien/.fzf/shell/key-bindings.bash"
+source "/home/sglavoie/.fzf/shell/key-bindings.bash"
