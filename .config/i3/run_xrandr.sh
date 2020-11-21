e9af02df64343daa0ed72e9895df133e38a56ce8
Sébastien Lavoie <sgdlavoie@gmail.com>
2020-11-21 09:43:07 -0600

    i3: Call script to set up displays to keep clean syntax without quotes
    * The `xrandr` command does not run successfully every time due to the
      fact that it was not surrounded by quotes, which isn't necessary when
      calling it from a shell script. Doing that instead 🤞.

---
+++ .config/i3/run_xrandr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set up monitor at launch automatically and set laptop screen as the
+# primary one. Set monitor to the top (being on the vertical axis
+# makes it possible to focus bottom/top and jump from one monitor to
+# the next, which is not possible if the monitor is set to top-left or
+# top-right in diagonal, for instance)
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 \
+    --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output DisplayPort-0 --off --output DisplayPort-1 --off
