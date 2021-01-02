654b76fe4580abde3517691b62132256567cbfc4
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-01-02 09:09:00 -0600

    Add script and bindings to toggle monitor off/on

--- .config/i3/run_xrandr.sh
+++ .config/i3/run_xrandr.sh
@@ -6,5 +6,4 @@
 # the next, which is not possible if the monitor is set to top-left or
 # top-right in diagonal, for instance)
 xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 \
-    --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DisplayPort-0 --off --output DisplayPort-1 --off
+    --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
---
+++ .config/i3/run_xrandr_laptop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 \
+    --rotate normal --output HDMI-A-0 --off
