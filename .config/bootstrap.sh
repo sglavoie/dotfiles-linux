e405378246ca8a769a9f415a957a4e09d719d006
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-06-04 19:05:35 -0500

    Remove symlinks for Git in bootstrap.sh
    * Those are not useful. The main ~/.gitconfig file includes my personal
      config at the top and that personal config in turn includes my work
      config as necessary, so only this ~/.config is needed in the dotfiles.

--- .config/bootstrap.sh
+++ .config/bootstrap.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Fonts
 ln -nsf ~/Dropbox/.custom/fonts ~/.fonts
-
-# Git
-ln -sf ~/Dropbox/.custom/.gitconfig ~/.gitconfig
-ln -sf ~/Dropbox/.custom/.gitconfig-github ~/.gitconfig-github
-ln -sf ~/Dropbox/.custom/.gitconfig-datopian ~/.gitconfig-datopian
